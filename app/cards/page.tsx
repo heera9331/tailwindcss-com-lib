06b91d1c2f2ec1ad26316764f9aa41496d5edbba
heera9331 <heera9331@gmail.com>
2024-03-24 00:26:06 +0530

    yt card

--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -60,8 +60,6 @@ const Page = () => {
                     <div>
                         <button className="bg-blue-700 text-white rounded-sm py-1 px-2 hover:bg-blue-600">Create new job</button>
                     </div>
-
-
                 </div>
                 <div className="py-2">
                     <h2 className="font-semibold text-gray-600">Back End Developer</h2>
@@ -75,6 +73,23 @@ const Page = () => {
                     <div className="flex gap-2 items-center py-2">
                         <IoPeopleSharp className="text-xl" />
                         <span className="text-sm">Engineering</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="min-h-[100vh] text-white p-4" style={{ backgroundColor: "rgb(40 40 40)" }}>
+                {/* cards  */}
+                <div className="max-w-[350px] px-5 rounded-md border-2" style={{ backgroundColor: "rgb(31 31 31)", borderColor: "rgb(62 62 62)" }}>
+                    <div className="pt-4 pb-2">
+                        <h2 className="text-2xl font-semibold">Card Heading</h2>
+                    </div>
+                    {/* body */}
+                    <div className="flex flex-col gap-2">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A dolore dolor eos excepturi nihil fugiat accusamus rem, veniam nisi obcaecati ratione in quasi deserunt fugit corrupti magnam inventore distinctio necessitatibus! Quo accusantium laudantium corrupti nobis id non quos sit.</p>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, illo.</p>
+                    </div>
+                    <div className="pb-4">
+
                     </div>
                 </div>
             </div>
